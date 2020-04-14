5ce3a05b45f511a79614bdce484b7ecebf6cbaad
GazApp <juangaz@hotmail.com>
2020-04-14 16:54:00 -0500

    Commit test debug

---
+++ Test/src/suma/add.java
@@ -0,0 +1,27 @@
+package suma;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 5;
+		int b = 8;
+		int suma = a + b;
+		
+		/*
+		 * for (int i = 0; i <4; i++) { System.out.println("valor de : "+i); }
+		 * 
+		 */
+	
+		
+		if ( suma<12) {
+			System.out.println("el resultado es menor a 10");
+
+		}else {
+			System.out.println("No es menor que 10");
+		}
+
+	}
+
+}
